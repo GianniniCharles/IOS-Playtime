0c38e5ebc2631df7c5102d7b79cd8b24bf0c7ee4
Giannini Charles <gcharles@oceanreef.com>
2018-10-21 20:33:06 -0400

    Xylophone app now plays

--- Client/Xylophone-iOS12/Xylophone/ VIewController.swift
+++ Client/Xylophone-iOS12/Xylophone/ VIewController.swift
@@ -7,9 +7,11 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController{
+class ViewController: UIViewController, AVAudioPlayerDelegate{
     
+    var audioPlayer: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +20,18 @@ class ViewController: UIViewController{
 
 
     @IBAction func notePressed(_ sender: UIButton) {
+        
+        let soundURL = Bundle.main.url(forResource: "note\(sender.tag)", withExtension: "wav")!
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+        } catch {
+            print(error)
+        }
+        
+        audioPlayer.play()
+        
+        
         
         
         
