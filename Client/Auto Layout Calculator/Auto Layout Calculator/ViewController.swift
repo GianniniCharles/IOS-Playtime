1a5a9c1ec9578c488670a3484c43884698ccd0af
Giannini Charles <gcharles@oceanreef.com>
2018-11-09 20:05:08 -0500

    Calculator auto layout (no it doesn't calculate lol. The idea was to layout the buttons)

---
+++ Client/Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Giannini Charles on 11/9/18.
+//  Copyright © 2018 xcoderj9. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+    }
+}
+
