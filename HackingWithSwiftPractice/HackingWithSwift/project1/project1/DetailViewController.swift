fc2d81ed243483d2576418bd12ee3eb5376d0424
Giannini Charles <gcharles@oceanreef.com>
2018-12-01 20:59:03 -0500

    HackingWithSwiftProject1 Completed. Covered constants and variables, method overrides, table views and image views, app bundles, FileManager, typecasting, arrays, loops, optionals, view controllers, storyboards, outlets, Auto Layout, UIImage and more

---
+++ HackingWithSwiftPractice/HackingWithSwift/project1/project1/DetailViewController.swift
@@ -0,0 +1,53 @@
+//
+//  DetailViewController.swift
+//  project1
+//
+//  Created by Giannini Charles on 12/1/18.
+//  Copyright © 2018 exiaj9. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    
+    override var prefersHomeIndicatorAutoHidden: Bool {
+        return navigationController?.hidesBarsOnTap ?? false
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.largeTitleDisplayMode = .never
+        title = selectedImage
+
+        // Do any additional setup after loading the view.
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
